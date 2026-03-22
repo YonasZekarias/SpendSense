13f0c98410c6375296e2e4ee78a87bcac820372c
YonasAdane <yonasadane94@gmail.com>
2026-03-22 08:51:40 +0300

    chore: update package.json and server.ts for improved module handling and remove unused dependencies

--- apps/realtime/src/server.ts
+++ apps/realtime/src/server.ts
@@ -1,15 +1,14 @@
-import { json, urlencoded } from "body-parser";
+import cors from "cors";
 import express, { type Express } from "express";
 import morgan from "morgan";
-import cors from "cors";
 
 export const createServer = (): Express => {
   const app = express();
   app
     .disable("x-powered-by")
     .use(morgan("dev"))
-    .use(urlencoded({ extended: true }))
-    .use(json())
+    .use(express.urlencoded({ extended: true }))
+    .use(express.json())
     .use(cors())
     .get("/message/:name", (req, res) => {
       return res.json({ message: `hello ${req.params.name}` });
